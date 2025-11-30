70d9a27d496bb4d1455273011137c44b4e94f016
JaymentalEntertainment <djohnnyhoffmann@gmail.com>
2025-11-30 01:46:20 +0100

    TulpaKingImplement Version 0.1 from Jaymental

---
+++ public/js/TulpaKing.js
@@ -0,0 +1,33 @@
+/*CSS-Loader
+loadStylesheet('/css/TulpaKing.css');
+//End
+
+//Vars
+let Player = {
+    Gold: 5000,
+};
+
+//Helper-Functions
+function toggleMenu() {
+    document.getElementById('Menu').classList.toggle('hidethis');
+    document.getElementById('Menucontent').classList.toggle('hidethis', true);
+}
+
+function toggleInfo() {
+    const Menucontent = document.getElementById('Menucontent');
+    let html = `
+        <h1>Spielerinformationen</h1>
+        <p>Name: ${Player.Name}</p>
+        <p>Gold: ${Player.Gold}</p>
+    `;
+    Menucontent.innerHTML = html;
+    Menucontent.classList.toggle('hidethis', false);
+}
+function toggleTulpas() {
+
+}
+function toggleTulpLex() {
+
+}
+
+*/
